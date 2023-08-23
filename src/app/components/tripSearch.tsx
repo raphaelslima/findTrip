44fb6ce0783ea77340c540e1e1b052b2b33167bc
Raphael Lima <raphaeldesousalm@gmal.com>
2023-08-23 14:50:38 -0300

    feat: add button in search

--- src/app/components/tripSearch.tsx
+++ src/app/components/tripSearch.tsx
@@ -4,11 +4,12 @@ import React from "react";
 import Input from "./Input";
 import DatePicker from "./DatePicker";
 import CurrencyInput from "./CurrencyInput";
+import Button from "./Button";
 
 
 const TripSearch = ()=>{
     return(
-        <div className="container mx-auto p-5">
+        <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
             <h1 className="font-semibold text-2xl text-primaryDark text-center">
                 Encontre sua sua <span className="text-primary">viagem!</span>
             </h1>
@@ -17,7 +18,10 @@ const TripSearch = ()=>{
                 <Input placeholder="Onde você quer ir?" className="w-full"/>
                 <DatePicker placeholderText="Data de Ida" onChange={()=> {}} className="w-full"/>
                 <CurrencyInput placeholder="Orçamento" className="w-full"/>
+                <Button>Buscar</Button>
             </div>
+
+           
         </div>
 
     )
